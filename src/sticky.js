af49b04c596f9e939b021e28a65d561f61c7d2d1
Javier <javier.medina15@gmail.com>
2023-05-06 00:25:03 -0400

    Initial commit

---
+++ src/sticky.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector(".nav");
+const main = document.querySelector(".main");
+const navHeight = nav.offsetHeight;
+const sticky = nav.offsetTop;
+
+export default function stickyHeader() {
+  const currentScrollPos = window.pageYOffset;
+
+  if (currentScrollPos > sticky) {
+    nav.style.position = "fixed";
+    nav.style.top = 0;
+    nav.style.width = "100%";
+    main.style.paddingTop = `${navHeight}px`;
+  } else {
+    nav.style.position = "static";
+    main.style.paddingTop = 0;
+    nav.style.width = "auto";
+  }
+}
