9654fc324c6c0bcf029086df4e567e4dec496f4a
Javier <javier.medina15@gmail.com>
2023-05-07 21:48:02 -0400

    Initial commit

---
+++ src/sticky-scroll.js
@@ -0,0 +1,22 @@
+let navBarPosition = 0;
+let oldScrollY = 0;
+const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
+const main = document.querySelector(".main");
+const headerHeight = header.offsetHeight;
+const sticky = nav.offsetTop;
+
+main.style.paddingTop = `${headerHeight}px`;
+header.style.position = "fixed";
+header.style.width = "100%";
+
+export default function stickyScrollHeader() {
+  const currentScrollY = window.scrollY;
+  const scrollAmount = currentScrollY - oldScrollY;
+
+  navBarPosition += scrollAmount;
+  navBarPosition = Math.max(0, Math.min(sticky,navBarPosition));
+  oldScrollY = currentScrollY;
+
+  header.style.top = `-${navBarPosition}px`;
+}
