a7a64360fc9a4760f2abbbb0f44407ae56211616
Javier <javier.medina15@gmail.com>
2023-05-09 22:35:54 -0400

    Create the structure & style of the new list page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import "./style.css";
 import "./checkbox.css";
 import stickyScrollHeader from "./sticky-scroll";
+import {
+  newListButton,
+  newListFormOpen,
+  newListButtonClose,
+  newListFormClose
+} from "./todo";
 
 window.addEventListener("scroll", stickyScrollHeader);
+
+newListButton.addEventListener("click", newListFormOpen);
+newListButtonClose.addEventListener("click", newListFormClose)
--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,18 @@
-const printHello = () => {
-  console.log('hello');
-};
+const aside = document.querySelector(".aside");
 
-const printHi = () => {
-  console.log('hi');
-};
+const newListButton = document.querySelector(".new-list");
+const newListFormOpen = () => {
+  aside.style.display = "flex";
+}
+
+const newListButtonClose = document.querySelector(".aside__close");
+const newListFormClose = () => {
+  aside.style.display = "none";
+}
 
 export {
-  printHello,
-  printHi
+  newListButton,
+  newListFormOpen,
+  newListButtonClose,
+  newListFormClose
 };
