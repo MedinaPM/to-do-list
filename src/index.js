688bc3ef3c5444fc0d30bf98d3af1fa16bc66798
Javier <javier.medina15@gmail.com>
2023-05-10 22:03:58 -0400

    Create the structure & style of new task pop-up

--- src/index.js
+++ src/index.js
@@ -12,3 +12,21 @@ window.addEventListener("scroll", stickyScrollHeader);
 
 newListButton.addEventListener("click", newListFormOpen);
 closeButton.addEventListener("click", newListFormClose);
+
+
+
+
+const footerAddButton = document.querySelector(".footer__add-button");
+const popUp = document.querySelector(".pop-up");
+const popUpBackground = document.querySelector(".pop-up__background");
+
+const addNewTask = () => {
+  popUp.style.display = "flex";
+}
+const closePopUp = () => {
+  popUp.style.display = "none";
+}
+
+
+footerAddButton.addEventListener("click", addNewTask);
+popUpBackground.addEventListener("click", closePopUp);
