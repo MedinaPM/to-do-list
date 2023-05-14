1b8187141db6989ceb9f86e4d38b1add65ec6f00
Javier <javier.medina15@gmail.com>
2023-05-13 21:15:57 -0400

    Modify the Task factory to save the list name

--- src/tasks.js
+++ src/tasks.js
@@ -4,25 +4,36 @@ import { closePopUp } from "./display";
 const myTasks = [];
 
 // factory to create new tasks
-const Task = (name, detail) => {
+const Task = (name, detail, list) => {
   const getName = () => name;
   const getDetail = () => detail;
+  const getList = () => list;
 
-  return { getDetail, getName };
+  return { getDetail, getName, getList };
 };
 
 // task object manipulation
 export default function submitNewTask() {
   const formTask = document.querySelector("#new-task-input");
   const formDetail = document.querySelector("#add-details-input");
-  const newTask = Task(formTask.value, formDetail.value);
+  const activeList = document.querySelector(".nav-bar_active");
+
+  const newTask = Task(
+    formTask.value,
+    formDetail.value,
+    activeList.textContent
+  );
+
   const taskForm = document.querySelector("#task-form");
+  taskForm.reset();
 
   myTasks.push(newTask);
-  taskForm.reset();
+  
   closePopUp();
 
-  console.log(myTasks);
-  console.log(myTasks[0].getName());
-  console.log(myTasks[0].getDetail());
+  myTasks.forEach(task => {
+    console.log(task.getName());
+    console.log(task.getDetail());
+    console.log(task.getList());
+  });
 }
